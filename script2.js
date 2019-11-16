7ead28d3861687da8dfcedcf3c022c71a2867892
renan <renanalves23@outlook.com>
2019-11-16 10:32:00 -0300

    Add arquivo script2.js Exemplo de .forEach

--- script2.js
+++ script2.js
@@ -1,8 +1,23 @@
 //USOS DE FOR E .FOREACH()
 
+//array simples
+    const array = [1,2,3,4];
 //for acessa tanto prototype Array, como tbm prototype HtmlCollection
     //sintaxe:
-      
+  //for (let i = 0; i < array.length; i++)
 
-//
+//.forEach()
+  //sintaxe:
+  /* 
+  array.forEach((a) => {
+    console.log(a)
+  });
+   */
+
+  
+const arr = array.forEach((element) => {
+  return element;
+})
+console.log(arr)  
+
 
